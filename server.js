c4b8c43c651ca1332fff131813367e652313c5c6
NovaShang <styleshang@yeah.net>
2017-02-27 20:25:21 +0800

    ok

--- server.js
+++ server.js
@@ -2,19 +2,18 @@
 var express = require('express');
 var Sequelize = require('sequelize');
 var swig = require('swig');
-
-//============================DataModel========================
+var bodyparser = require('body-parser');
+var repl = require('repl');
+//============================DataModel==========================
 //Initialize ORM framework
 var sequelize = new Sequelize('blog', null, null, { dialect: 'sqlite', storage: 'blog.db' });
 //Define data models
 var Article = sequelize.define('article', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     title: Sequelize.TEXT,
     summary: Sequelize.TEXT,
     content: Sequelize.TEXT,
 });
 var Tag = sequelize.define('tag', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     name: Sequelize.STRING
 });
 var Category = sequelize.define('category', {
@@ -23,49 +22,68 @@ var Category = sequelize.define('category', {
 });
 //Build assosiation relationship between models
 Article.belongsTo(Category);
-
-//===========================WebServer==========================
+//===========================WebServer===========================
 //Initialize and setup web framework
 var app = express();
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.use('/static', express.static('static'));
-// Routers
-app.get('/', function (request, response) {
+app.use(bodyparser())
+    // Routers
+app.get('/', function(request, response) {
     response.render('home.html');
 });
-app.get('/blog', function (request, response) {
+app.get('/blog', function(request, response) {
     response.render('blog.html', {
         title: 'BIM & Coding',
         sub_title: '高尚的博客',
     });
 });
-app.get('/blog/publish', function (request, response) {
-    response.render('publish.html');
+app.get('/blog/publish', function(request, response) {
+    response.render('publish.html', { cates: Category.findAll() });
 });
-app.get('/blog/:id', function (request, response) {
+app.post('/api/blog/publish', function(request, response) {
+    Promise.all([
+            Article.create({ title: request.body.title, summary: request.body.summary, content: request.body.content }),
+            Category.findById(request.body.cate_id)
+        ])
+        .then(x => x[0].setCategory(x[1]), x => response.json({ result: "failed", message: x[0] + x[1] }))
+        .then(() => response.json({ result: "success", url: "/blog/" }), x => response.json(x));
+});
+app.get('/api/blog/tags', function(request, response) {
+    Tag.findAll().then(x => response.json(x))
+});
+app.post('/api/blog/tags', function(request, response) {
+    Tag.create({ name: request.body.name })
+        .then(x => response.json({ result: "success", url: "" }), x => response.json(x));
+});
+app.get('/api/blog/categories', function(request, response) {
+    Category.findAll().then(x => response.json(x))
+});
+app.post('/api/blog/categories', function(request, response) {
+    Category.create({ name: request.body.name })
+        .then(x => response.json({ result: "success", url: "" }), x => response.json(x));
+});
+app.get('/blog/:id', function(request, response) {
     response.render('article.html');
 });
-app.get('/blog/tag/:id', function (request, response) {
+app.get('/blog/tag/:id', function(request, response) {
     response.render('blog.html');
 });
-app.get('/blog/cate/:name', function (request, response) {
+app.get('/blog/cate/:name', function(request, response) {
     response.render('blog.html');
 });
-app.get('/about', function (request, response) {
-    response.render('about.html');
+app.get('/about', function(request, response) {
+    response.json({ asdf: "asdfasfd" });
 });
-app.get('/portfolio', function (request, response) {
+app.get('/portfolio', function(request, response) {
     response.render('portfolio.html');
 });
-
-//=============================Run==============================
+//===============================Run==============================
 if (process.argv[2] == 'migrate') {
     //Sync database
     console.log('Syncing database')
-    sequelize.sync().then(
-        console.log('Database sync complete!')
-    );
+    sequelize.sync().then(console.log('Database sync complete!'));
 } else {
     // Run server
     app.listen(8000);
