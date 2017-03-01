dc6bcf65414513a19ae2fc6d904cb005de1ef3c0
NovaShang <styleshang@yeah.net>
2017-03-01 12:46:03 +0800

    lalallala

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var repl = require('repl');
 //============================DataModel==========================
 
 //Initialize ORM framework
-var sequelize = new Sequelize('blog', null, null, { dialect: 'sqlite', storage: 'blog.db' });
+var sequelize = new Sequelize('blog', null, null, {
+    dialect: 'sqlite',
+    storage: 'blog.db'
+});
 
 //Define data models
 var Article = sequelize.define('article', {
@@ -45,7 +48,11 @@ app.get('/', function(request, response) {
 });
 // Blog index
 app.get('/blog', function(request, response) {
-    Promise.all([Article.findAll({ include: [Tag] }), Tag.findAll(), Category.findAll()])
+    Promise.all([
+            Article.findAll({ include: [Tag] }),
+            Tag.findAll(),
+            Category.findAll()
+        ])
         .then(x => response.render('blog.html', {
             title: 'BIM & Coding',
             sub_title: '高尚的博客',
@@ -54,13 +61,13 @@ app.get('/blog', function(request, response) {
             cates: x[2]
         }));
 });
-// Article page
-app.get('/blog/:id', function(request, response) {
-    response.render('article.html');
-});
 // Index of a tag
 app.get('/blog/tag/:id', function(request, response) {
-    Promise.all([Tag.findById(request.params.id, { include: [Article] }), Tag.findAll(), Category.findAll()])
+    Promise.all([
+            Tag.findById(request.params.id, { include: [Article] }),
+            Tag.findAll(),
+            Category.findAll()
+        ])
         .then(x => response.render('blog.html', {
             title: x[0].name,
             sub_title: '标签',
@@ -71,22 +78,54 @@ app.get('/blog/tag/:id', function(request, response) {
 });
 // Index of a category
 app.get('/blog/cate/:name', function(request, response) {
-    Promise.all([Category.findOne({ where: { name: request.params.name } }, { include: [Article] }), Tag.findAll(), Category.findAll()])
+    Promise.all([
+            Article.findAll({
+                include: [{
+                    model: Category,
+                    where: { name: request.params.name }
+                }]
+            }),
+            Tag.findAll(),
+            Category.findAll()
+        ])
         .then(x => response.render('blog.html', {
             title: x[0].title,
             sub_title: '类别',
-            articles: x[0].articles,
+            articles: x[0],
             tags: x[1],
             cates: x[2]
         }));
 });
+// Search result
+app.get('/blog/search', function(request, response) {
+    Promise.all([
+            Article.findAll({
+                where: { title: { $like: '%' + request.query.keyword + '%' } }
+            }),
+            Tag.findAll(),
+            Category.findAll()
+        ])
+        .then(x => response.render('blog.html', {
+            title: request.query.keyword,
+            sub_title: '搜索结果',
+            articles: x[0],
+            tags: x[1],
+            cates: x[2]
+        }));
+});
+// Article page
+app.get('/blog/:id', function(request, response) {
+    response.render('article.html');
+});
 // Blog editor
 app.get('/blog/publish', function(request, response) {
-    response.render('publish.html', { cates: Category.findAll() });
+    response.render('publish.html');
 });
 // About page
 app.get('/about', function(request, response) {
-    response.json({ asdf: "asdfasfd" });
+    response.json({
+        asdf: "asdfasfd"
+    });
 });
 // Portfolio page
 app.get('/portfolio', function(request, response) {
@@ -98,23 +137,51 @@ app.get('/portfolio', function(request, response) {
 app.post('/api/blog/publish', function(request, response) {
     var article;
     Promise.all([
-            Promise.all(request.body.tags.map(x => Tag.findOne({ where: { name: x } }))),
-            Promise.all([Article.create({ title: request.body.title, summary: request.body.summary, content: request.body.content })
+            Promise.all(
+                request.body.tags.map(
+                    x => Tag.findOne({ where: { name: x } })
+                )),
+            Promise.all([
+                Article.create({
+                    title: request.body.title,
+                    summary: request.body.summary,
+                    content: request.body.content
+                })
                 .then(x => article = x),
-                Category.findOne({ where: { name: request.body.cate } })
-            ]).then(x => article.setCategory(x[1]), x => response.json({ result: "failed", message: x[0] + x[1] })),
+                Category.findOne({
+                    where: { name: request.body.cate }
+                })
+            ])
+            .then(
+                x => article.setCategory(x[1]),
+                x => response.json({
+                    result: "failed",
+                    message: x[0] + x[1]
+                })),
         ])
-        .then(x => article.setTags(x[0]), x => response.json({ result: "failed", message: x[0] + x[1] }))
-        .then(() => response.json({ result: "success", url: "/blog/" + article.id }), x => response.json(x));
+        .then(x => article.setTags(x[0]),
+            x => response.json({
+                result: "failed",
+                message: x[0] + x[1]
+            }))
+        .then(() => response.json({
+            result: "success",
+            url: "/blog/" + article.id
+        }), x => response.json(x));
 });
 // Get tags
 app.get('/api/blog/tags', function(request, response) {
-    Tag.findAll().then(x => response.json(x))
+    Tag.findAll()
+        .then(x => response.json(x))
 });
 // Add a tag
 app.post('/api/blog/tags', function(request, response) {
+
     Tag.create({ name: request.body.name })
-        .then(x => response.json({ result: "success", url: "" }), x => response.json(x));
+        .then(x => response.json({
+            result: "success",
+            url: ""
+        }), x => response.json(x));
 });
 // Get categories
 app.get('/api/blog/categories', function(request, response) {
@@ -122,8 +189,14 @@ app.get('/api/blog/categories', function(request, response) {
 });
 // Add a category
 app.post('/api/blog/categories', function(request, response) {
-    Category.create({ name: request.body.name, title: request.body.title })
-        .then(x => response.json({ result: "success", url: "" }), x => response.json(x));
+    Category.create({
+            name: request.body.name,
+            title: request.body.title
+        })
+        .then(x => response.json({
+            result: "success",
+            url: ""
+        }), x => response.json(x));
 });
 
 //===============================Run==============================
