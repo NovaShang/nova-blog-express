3fc7633f0947d3666ea09be04c3df38ed5f41c02
NovaShang <styleshang@yeah.net>
2017-03-02 19:28:21 +0800

    new

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var Sequelize = require('sequelize');
 var swig = require('swig');
 var bodyparser = require('body-parser');
 var repl = require('repl');
-var marked = require('marked')
-    //============================DataModel==========================
+var marked = require('marked');
+//============================DataModel==========================
 
 //Initialize ORM framework
 var sequelize = new Sequelize('blog', null, null, {
@@ -40,6 +40,12 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.use('/static', express.static('static'));
 app.use(bodyparser());
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", '*');
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    next();
+})
 
 // Pages
 // Home page
